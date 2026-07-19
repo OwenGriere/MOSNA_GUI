cb254334801d701a19e427e8e0d3009c0d7506fc
Owen GRIERE <owen.griere@gmail.com>
2026-07-19 19:06:42 +0200

    debugging without reduction

--- package/core/NAS/plot_embedding.py
+++ package/core/NAS/plot_embedding.py
@@ -3,9 +3,11 @@ from mosna import mosna
 
 def plot_embedding(embedding_path, cluster_labels, save_dir, cluster_params):
 
-    embedding = np.load(embedding_path)
-
-    mosna.plot_clusters(embedding,
-                        cluster_labels,
-                        save_dir,
-                        cluster_params)
+    if embedding_path.exists():
+        embedding = np.load(embedding_path)
+        mosna.plot_clusters(embedding,
+                            cluster_labels,
+                            save_dir,
+                            cluster_params)
+    else:
+        return None
